238bda2a71cfab5724f01711dcab47ee2b08f992
harithtech <harith.trmz@gmail.com>
2022-04-09 10:50:29 +0800

    1. make responsive app bar

---
+++ src/components/NavBar/MenuAppBar.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import { useNavigate } from 'react-router-dom';
+import { routeNames } from '../../todo/routes/routesName';
+
+ export const ResponsiveAppBar = () => {
+
+  const navigate = useNavigate();
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
+          >
+            TODO APP
+          </Typography>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Profile">
+              <IconButton onClick={() => navigate(routeNames.ABOUT)} sx={{ p: 0 }}>
+                <Avatar alt="Harith" src={`${process.env.PUBLIC_URL}/assets/images/rith.png`} />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
--- src/todo/pages/Todo.js
+++ src/todo/pages/Todo.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { ResponsiveAppBar } from '../../components'
 
 export const TodoPage = () => {
 	return (
+		<>
+		<ResponsiveAppBar />
 		<div>
 			<h1>This is Todo App</h1>
 		</div>
+		</>
 	)
 }
